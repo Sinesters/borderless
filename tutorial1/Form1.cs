953d3f641fa4676741fa61af504dc9ca17c6a039
David Teixeira <davidtei3d@gmail.com>
2019-10-08 15:09:37 +0100

    borderless whit controls

--- tutorial1/Form1.cs
+++ tutorial1/Form1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,9 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-// This is the code for your desktop app.
-// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 
 namespace tutorial1
 {
@@ -20,16 +17,11 @@ namespace tutorial1
             InitializeComponent();
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            // Click on the link below to continue learning how to build a desktop app using WinForms!
-            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Thanks!");
+            BackColor = Color.Black;
+            TransparencyKey = Color.Black;
+            chages
         }
     }
 }
